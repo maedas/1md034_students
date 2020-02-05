5c3e82df07937b1c984c71e177e59a149c38c010
maedas <hugo.sandelin@gmail.com>
2020-02-05 14:28:02 +0100

    4-3

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,1 +1,21 @@
-ÿþ
+function menuItem(name, img, kCal, all) {
+    this.name = name; // The this keyword refers to the object itself
+    this.img = img;
+    this.kCal = kCal;
+    this.Allergens = all;
+    this.name = function() {
+        return this.name + ' - ' + this.kCal;
+    };
+}
+
+let og =  new menuItem('The Original', /img/rawburger.jpg, 'Yes.', 'Cheese & Gluten');
+let ex =  new menuItem('The Extreme', /img/vegburger.jpg, 'This is not your main problem', 'Ron Swanson is allergic to this');
+let gr =  new menuItem('The Gordon Ramsay', /img/ramsayburger.jpg, 'Only on Tuesdays.', 'Cheese & Gluten');
+let wh =  new menuItem('The "why are you here?"', /img/burger.jpg, 'Only on Tuesdays.', 'Cheese & Gluten');
+let ss =  new menuItem('The Sporty Spice', /img/sneakerburger.jpg, 'Only on Tuesdays.', 'Cheese & Gluten');
+
+console.log(og.name());
+console.log(ex.name());
+console.log(gr.name());
+console.log(wh.name());
+console.log(ss.name());
